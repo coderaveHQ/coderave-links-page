7657e956b936d288b2d4bbfd7a0ec82bbb059327
sroepges <47949518+sroepges@users.noreply.github.com>
2024-08-12 23:01:06 +0200

    fix: z index

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import SocialsCard from "@/components/socials-card"
 function App() {
   return (
     <>
-      <div className="h-[50rem] -z-20 min-h-screen w-full dark:bg-neutral-950 bg-neutral-50  dark:bg-dot-neutral-50/[0.2] bg-dot-neutral-950/[0.2] relative flex flex-col items-center justify-center">
-        {/* Radial gradient for the container to give a faded look */}
-        <div className="absolute -z-10 pointer-events-none inset-0 flex items-center justify-center dark:bg-neutral-950 bg-neutral-50 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="h-[50rem] min-h-screen w-full dark:bg-neutral-950 bg-neutral-50  dark:bg-dot-neutral-50/[0.2] bg-dot-neutral-950/[0.2]">
+        <div className="absolute inset-0 dark:bg-neutral-950 bg-neutral-50 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div className="z-20 min-h-screen relative flex flex-col items-center justify-center">
+          {/* Header Section */}
+          <Header />
 
-        {/* Header Section */}
-        <Header />
-
-        {/* Socials Section */}
-        <SocialsCard />
+          {/* Socials Section */}
+          <SocialsCard />
+        </div>
       </div>
     </>
   )
