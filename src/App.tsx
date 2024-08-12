a6a7432f638ae58fd559e10c959ef113f6ba337a
sroepges <47949518+sroepges@users.noreply.github.com>
2024-08-12 22:48:17 +0200

    style: add dot bg

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import SocialsCard from "@/components/socials-card"
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-neutral-50 dark:bg-neutral-950 p-4">
+      <div className="h-[50rem] -z-20 min-h-screen w-full dark:bg-neutral-950 bg-neutral-50  dark:bg-dot-neutral-50/[0.2] bg-dot-neutral-950/[0.2] relative flex flex-col items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute -z-10 pointer-events-none inset-0 flex items-center justify-center dark:bg-neutral-950 bg-neutral-50 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+
         {/* Header Section */}
         <Header />
 
